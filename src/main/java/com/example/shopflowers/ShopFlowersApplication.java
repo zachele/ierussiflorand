c6ffdea315345659a67369d920753b514ef627b6
ierussi zachele <zachele.ierussi.2004@gmail.com>
2026-04-08 22:35:54 +0200

    Restore database runtime configuration and application startup

--- src/main/java/com/example/shopflowers/ShopFlowersApplication.java
+++ src/main/java/com/example/shopflowers/ShopFlowersApplication.java
@@ -11,7 +11,7 @@ public class ShopFlowersApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        AppConfig.setMode(AppMode.FULL); // cambia in DEMO o FILE per testare le altre modalità
+        AppConfig.setMode(AppMode.DEMO); // cambia in DEMO o FILE per testare le altre modalità
 
         FXMLLoader fxmlLoader = new FXMLLoader(
                 ShopFlowersApplication.class.getResource("/com/example/shopflowers/login-view.fxml")
